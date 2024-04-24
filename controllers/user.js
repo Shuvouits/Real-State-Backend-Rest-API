339c91c7b3e7fbfadea168a977274e4a8236a252
Shuvo Bhowmik <41848224+Shuvouits@users.noreply.github.com>
2024-04-24 12:18:37 +0600

    Update SignUp

--- controllers/user.js
+++ controllers/user.js
@@ -4,27 +4,26 @@ const User = require('../models/user.js');
 const Listing = require('../models/listing.js');
 
 exports.signup = async (req, res) => {
-
     try {
         const { username, email, password } = req.body;
+
         const usernameCheck = await User.findOne({ username: username })
         if (usernameCheck) {
             return res.status(401).json({
-                "message": "username are already used",
-            })
-
+                "message": "Username is already used",
+            });
         }
 
         const emailCheck = await User.findOne({ email: email })
         if (emailCheck) {
             return res.status(401).json({
-                "message": "email are already used",
-            })
-
+                "message": "Email is already used",
+            });
         }
-        const cryptedPassword = await bcrypt.hash(password, 12)
+
+        const cryptedPassword = await bcrypt.hash(password, 12);
+
         const user = await new User({
-            id,
             username,
             password: cryptedPassword,
             email: email
@@ -33,20 +32,14 @@ exports.signup = async (req, res) => {
         return res.status(200).json({
             "message": "New user created successfully",
             user
-        })
-
-
-
-
+        });
     } catch (error) {
-        res.status(500).json({
+        console.log(error);
+        return res.status(500).json({
             "message": "Internal Server Problem"
-        })
-
+        });
     }
-
-
-}
+};
 
 exports.signin = async (req, res) => {
     try {
